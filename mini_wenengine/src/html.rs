e024d09f5617e413f30334fe6cb62b5b4143f771
PeterWright <peterwrighten@gmail.com>
2021-12-19 16:20:51 +0900

    html.rs

--- mini_wenengine/src/html.rs
+++ mini_wenengine/src/html.rs
@@ -21,4 +21,57 @@ impl Parser {
     fn eof(&self) -> bool {
         self.pos >= self.input.len()
     }
+
+    fn consume_char(&mut self) -> char {
+        let mut iter = self.input[self.pos..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.pos += next_pos;
+        return cur_char;
+    }
+
+    fn consume_while<F> (&mut self, test: F) -> String
+        where F: Fn(char) -> bool {
+            let mut result = String::new();
+            while !self.eof() && test(self.next_char()) {
+                result.push(self.consume_char());
+            }
+            return result;
+        }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(CharExt::is_whitespace);
+    }
+
+    fn parse_tag_name(&mut self) -> String {
+        self.consume_while(|c| match c {
+            'a'...'z' | 'A'...'Z' | '0'...'9' => true,
+            _ => false,
+        })
+    }
+
+    //In this version of HTML, a text node can contain any character except "<".
+    fn parse_node(&mut self) -> dom::Node {
+        match self.next_char() {
+            '<' => self.parse_element(), 
+            _ => self.parse_text()
+        }
+    }
+
+    fn parse_text(&mut self) -> dom::Node {
+        dom::text(self.consume_while(|c| c != '<'))
+    }
+
+    fn parse_element(&mut self) -> dom::Node {
+        //Opening tag.
+        assert!(self.consume_char() == '<');
+        let tag_name = self.parse_tag_name();
+        let attrs = self.parse_attributes();
+        assert!(self.consume_char() == '>');
+
+        //Contents.
+
+        
+    }
+
 }
